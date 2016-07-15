bb3962b6db250d1c5022558d5bf743fa5ef52eea
duupiimenta <eduardo.pimenta.bueno@outlook.com>
2016-07-15 01:55:41 -0300

    First Commit
    First commit, containing problems 1 and 2

---
+++ 2/2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 1;
+            int v2 = 2;
+            int v3 = v1 + v2;
+            int sum = 2;
+
+            while(v3 < 4000000)
+            {
+                if (v3 % 2 == 0)
+                    sum += v3;
+
+                v1 = v2;
+                v2 = v3;
+                v3 = v1 + v2;
+            }
+
+            Console.WriteLine(sum.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
